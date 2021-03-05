34bb86f7c810d29769abfe499c1476cfaac5cdc7
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2021-03-05 08:03:56 +0100

    some css

--- src/App.js
+++ src/App.js
@@ -1,51 +1,87 @@
-import React from "react";
+import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  NavLink
-} from "react-router-dom"
-import Home from './components/home'
-import Skills from './components/skills'
-import Interessen from './components/interessen'
-import Projekte from './components/projekte'
-import Footer from './components/footer'
+  NavLink,
+} from 'react-router-dom';
+import Home from './components/home';
+import Skills from './components/skills';
+import Interessen from './components/interessen';
+import Projekte from './components/projekte';
+import Footer from './components/footer';
 
-import GitHubIcon from '@material-ui/icons/GitHub'
-import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
 const App = () => {
-
   return (
     <Router>
-      <div className="background">
-        <nav className="navbar">
-          <NavLink to="/" exact={true} className="left" activeStyle={{ color: "rgb(255, 94, 0)" }}>clausing-lage</NavLink>
-          <NavLink to="/skills" className="right" activeStyle={{ color: "rgb(255, 94, 0)" }}>Skills</NavLink>
-          <NavLink to="/projekte" className="right" activeStyle={{ color: "rgb(255, 94, 0)" }}>Projekte</NavLink>
-          <NavLink to={'/interessen'} className="right" activeStyle={{ color: "rgb(255, 94, 0)" }}>Interessen</NavLink>
-          <a href="https://www.linkedin.com/feed/" target="_blank" className="right social"><LinkedInIcon /></a>
-          <a href="https://github.com/vClausingLage" target="_blank" className="right social"><GitHubIcon /></a>
+      <div className='background'>
+        <nav className='navbar'>
+          <NavLink
+            to='/'
+            exact={true}
+            className='left'
+            activeStyle={{ color: 'rgb(255, 94, 0)' }}
+          >
+            clausing-lage
+          </NavLink>
+          <NavLink
+            to='/skills'
+            className='right'
+            activeStyle={{ color: 'rgb(255, 94, 0)' }}
+          >
+            Skills
+          </NavLink>
+          <NavLink
+            to='/projekte'
+            className='right'
+            activeStyle={{ color: 'rgb(255, 94, 0)' }}
+          >
+            Projekte
+          </NavLink>
+          <NavLink
+            to={'/interessen'}
+            className='right'
+            activeStyle={{ color: 'rgb(255, 94, 0)' }}
+          >
+            Interessen
+          </NavLink>
+          <a
+            href='https://www.linkedin.com/feed/'
+            target='_blank'
+            className='right social'
+          >
+            <LinkedInIcon />
+          </a>
+          <a
+            href='https://github.com/vClausingLage'
+            target='_blank'
+            className='right social'
+          >
+            <GitHubIcon />
+          </a>
         </nav>
 
         <Switch>
-          <Route path="/skills" component={ Skills }>
+          <Route path='/skills' component={Skills}>
             <Skills />
           </Route>
-          <Route path="/interessen" component={ Interessen }>
+          <Route path='/interessen' component={Interessen}>
             <Interessen />
           </Route>
-          <Route path="/projekte" component={ Projekte }>
+          <Route path='/projekte' component={Projekte}>
             <Projekte />
           </Route>
-          <Route path="/" component={ Home }>
+          <Route path='/' component={Home}>
             <Home />
           </Route>
         </Switch>
-        </div>
+      </div>
       <Footer />
     </Router>
   );
-}
+};
 
 export default App;
--- src/components/home.js
+++ src/components/home.js
@@ -1,19 +1,25 @@
-import * as React from 'react'
-import profile from '../pictures/me1.jpg'
+import * as React from 'react';
+import profile from '../pictures/me1.jpg';
 
-function Home () {
-    return (
-        <>
-            <div className="flex profile">
-                <div className="flex-item">
-                    <img src={profile} alt="Profilbild" className="profile-image"/>
-                </div>
-                <div className="flex-item typewriter">
-                    <p>Herzlich willkommen!</p>
-                </div>
-            </div>
-        </>
-    )
+function Home() {
+  return (
+    <>
+      <div className='flex profile'>
+        <div className='flex-item'>
+          <img
+            src={profile}
+            alt='Profilbild'
+            className='profile-image'
+            width='700px'
+            height='700px'
+          />
+        </div>
+        <div className='flex-item typewriter'>
+          <p>Herzlich willkommen!</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
